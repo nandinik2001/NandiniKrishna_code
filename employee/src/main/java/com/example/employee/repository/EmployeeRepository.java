164631040f030e934c6fce0d80345f69b35d7c12
nandinik2001 <91665411+nandinik2001@users.noreply.github.com>
2024-07-24 01:29:02 -0500

    Delete employee/src/main/java/com/example/employee/repository/EmployeeRepository.java

--- employee/src/main/java/com/example/employee/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.employee.model.emp;
-
-public interface EmployeeRepository extends JpaRepository<emp, Long> {
-}
